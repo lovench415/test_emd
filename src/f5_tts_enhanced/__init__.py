4a46e3dc22b274bd836ceb078a4099917ae64322
lovench415 <97780774+lovench415@users.noreply.github.com>
2026-02-19 18:22:42 +0900

    Delete src/f5_tts_enhanced directory

--- src/f5_tts_enhanced/__init__.py
+++
@@ -1,3 +0,0 @@
-"""F5-TTS Enhanced: Cross-Language Zero-Shot + Automatic Emotion Transfer"""
-
-__version__ = "0.1.0"
--- src/f5_tts_enhanced/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .multilingual_vocab import build_multilingual_vocab, detect_language, save_vocab
-from .dataset import TTSDataset, FrameBatchSampler, create_dataloader
